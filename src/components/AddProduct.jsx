5b4ed77c9d19a603184152658e0c1704c8c16eac
aviConsole <avinash10june1993@gmail.com>
2025-04-11 10:26:28 +0530

    removed unused data

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,5 +1,4 @@
-import { React, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { React,  useState } from "react";
 
 const AddProduct = () => {
   const [name, setName] = useState("");
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -6,7 +6,7 @@ const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const navigate = useNavigate();
+  const navigate = useNavigate("/");
 
 
   useEffect(()=>{
